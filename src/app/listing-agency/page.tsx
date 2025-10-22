21493345b51765a2f6a7f90dcf3e3b51f2a71aa4
Yusuke Yamamoto <kodaifuku@gmail.com>
2025-10-23 02:32:32 +0900

    修正2

--- src/app/listing-agency/page.tsx
+++ src/app/listing-agency/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { motion } from "framer-motion";
